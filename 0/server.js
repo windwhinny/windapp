5bc8a646118db731eda66dd57fade606a6563f4f
windwhinny <windwhinny@gmail.com>
2013-09-05 18:39:42 -0700

    change default listen port to APP_PORT to support BAE

--- 0/server.js
+++ 0/server.js
@@ -39,7 +39,7 @@ require('./config/express')(app, config, passport);
 require('./config/routes')(app, passport, auth);
 
 //Start the app by listening on <port>
-var port = process.env.PORT || 3000;
+var port = process.env.APP_PORT || 3000;
 app.listen(port);
 console.log('Express app started on port ' + port);
 
