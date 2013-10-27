a1fc8601c6901ab1cd5dfe391ff475bba44077d3
baeadmin <baeadmin@baidu.com>
2013-10-28 00:52:41 +0800

    commit by edit_adapter

--- 0/config/express.js
+++ 0/config/express.js
@@ -9,9 +9,7 @@ var express = require('express'),
 
 var staticFileExtReg=/.(js|css|jpg|png|html|htm|ico)$/;
 module.exports = function(app, config, passport) {
-  var i=1;
     app.set('showStackError', true);
-console.log(i++);
 
     //Should be placed before express.static
     app.use(express.compress({
@@ -20,15 +18,11 @@ console.log(i++);
         },
         level: 9
     }));
-console.log(i++);
 
     //Setting the fav icon
     app.use(express.favicon());
     //handle static file
     var staticFileHandler=express.static(config.root+'/public');
-  
-  console.log(i++);
-
     app.use(function(req,res,next) {
         if(staticFileExtReg.test(req.url)){
           staticFileHandler(req,res,next); 
@@ -36,32 +30,26 @@ console.log(i++);
           next();
         }
     });
-console.log(i++);
 
     //Don't use logger for test env
     if (process.env.NODE_ENV !== 'test') {
         app.use(express.logger('dev'));
     }
-console.log(i++);
 
     //Set views path, template engine and default layout
     app.set('views', config.root + '/app/views');
     app.set('view engine', 'jade');
-console.log(i++);
 
     //Enable jsonp
     app.enable("jsonp callback");
-console.log(i++);
 
     app.configure(function() {
         //cookieParser should be above session
         app.use(express.cookieParser());
-console.log(i++);
 
         //bodyParser should be above methodOverride
         app.use(express.bodyParser());
         app.use(express.methodOverride());
-console.log(i++);
 
         //express/mongo session storage
         app.use(express.session({
@@ -71,20 +59,16 @@ console.log(i++);
                 collection: 'sessions'
             })
         }));
-console.log(i++);
 
         //connect flash for flash messages
         app.use(flash());
-console.log(i++);
 
         //dynamic helpers
         app.use(helpers(config.app.name));
-console.log(i++);
 
         //use passport session
         app.use(passport.initialize());
         app.use(passport.session());
-console.log(i++);
 
         //detected the request type
         app.use(function(req,res,next) {
@@ -104,11 +88,9 @@ console.log(i++);
             }
             next();
         });
-        console.log(i++);
-
+        
         //routes should be at the last
         app.use(app.router);
-console.log(i++);
 
         //Assume "not found" in the error msgs is a 404. this is somewhat silly, but valid, you can do whatever you like, set properties, use instanceof etc.
         app.use(function(err, req, res, next) {
@@ -123,7 +105,6 @@ console.log(i++);
                 message: err.message
             });
         });
-console.log(i++);
 
         //Assume 404 since no middleware responded
         app.use(function(req, res, next) {
