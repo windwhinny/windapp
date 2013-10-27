9e38834a64eccf1a51114cbabbd44c40e19b2e02
baeadmin <baeadmin@baidu.com>
2013-10-28 00:37:20 +0800

    commit by edit_adapter

--- 0/server.js
+++ 0/server.js
@@ -72,8 +72,7 @@ function runServer(){
 
 var http = require('http');
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+ 
   
   database(function(err){
     if(err){
