3a4445f46d5be44da5bdcbe7a7681c4fe12205c1
baeadmin <baeadmin@baidu.com>
2013-10-31 15:12:35 +0800

    commit by edit_adapter

--- 0/app/imageBrucket.js
+++ 0/app/imageBrucket.js
@@ -2,10 +2,12 @@ var qiniu=require('qiniu'),
     config=require('../config/config.js');
 qiniu.conf.ACCESS_KEY = config.imageBrucketAK;
 qiniu.conf.SECRET_KEY = config.imageBrucketSK;
-
-var putPolicy= new qiniu.rs.PutPolicy(config.imageBucketName);
-putPolicy.callbackBody = '{"size": $(fsize),"name":"$(etag)","type":"$(mimeType)","imageInfo":$(imageInfo)}';
 var brucketName=config.imageBrucketName;
+var putPolicy= new qiniu.rs.PutPolicy(brucketName);
+putPolicy.callbackBody = '{"size": $(fsize),"name":"$(etag)","type":"$(mimeType)","imageInfo":$(imageInfo)}';
+
+
+console.log()
 var imageBrucket={
   token:function(host,uid){
     putPolicy.callbackUrl = host+'/products/'+uid+'/image';
