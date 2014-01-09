8f9d057009856d97036ceaae313e801412c9123d
Eric Time <windwhinny@gmail.com>
2014-01-09 19:09:03 +0800

    remove loading deritive

--- 0/frontend/app.js
+++ 0/frontend/app.js
@@ -15,20 +15,45 @@ var app = angular.module('windapp',
   When there is XMLHttprequest opend, $rootScope will broadcast a message "ajaxStart",
   and when it ends, there is also a message "ajaxEnd". Used for globalAjax directive.
  */
-app.config(["$httpProvider","$rootScopeProvider",function($http,$rootScope) {
-  function getRootScope(){
-    $injector = angular.element(document).injector();
-    return $injector.get('$rootScope');
-  }
-  $http.defaults.transformRequest.push(function(data) {
-    getRootScope().$broadcast('ajaxStart')
-    return data;
-  });
+app.config([
+          "$httpProvider",'$provide',
+  function($httpProvider ,$provide) {
 
-  $http.defaults.transformResponse.push(function(data){
-    getRootScope().$broadcast('ajaxEnd');
-    return data;
-  })
+    function getRootScope(){
+      $injector = angular.element(document).injector();
+      return $injector.get('$rootScope');
+    };
+
+    function startAjax() {
+      getRootScope().$broadcast('ajaxStart')
+    };
+
+    function endAjax(){
+      getRootScope().$broadcast('ajaxEnd');
+    };
+
+  $provide.factory('HttpInterceptor', ['$q',function ($q) {
+    return {
+      request: function (config) {
+        startAjax(); 
+        return config || $q.when(config);
+      },
+      requestError: function (rejection) {
+        endAjax(); 
+        return $q.reject(rejection);
+      },
+      response: function (response) {
+        endAjax(); 
+        return response || $q.when(response);
+      },
+      responseError: function (rejection) {
+        endAjax(); 
+        return $q.reject(rejection);
+      }
+    };
+  }]);
+
+  $httpProvider.interceptors.push('HttpInterceptor');
 }]);
 
 return app;
