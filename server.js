154d6d12f9f6191c60208f055c997c3210b26610
Eric Time <windwhinny@gmail.com>
2014-01-16 15:50:53 +0800

    ..

--- server.js
+++ server.js
@@ -3,5 +3,6 @@ var port = 18080;
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write('<h1>Node.js</h1>');
+    res.write(JSON.stringify(process.env));
     res.end('<p>Hello World</p>');
 }).listen(port);
