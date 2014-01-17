d419b201d740e0350ea3e68f15e2c68667fbafa8
windwhinny <windwhinny@gmail.com>
2014-01-17 09:42:44 +0800

    test

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 /**
  * Module dependencies.
  */
-console.error(1);
+
 var express = require('express'),
     fs = require('fs'),
     passport = require('passport'),
     path=require('path');
 
-console.error(2);
+
 config = require('./config/config.js');
  
 
@@ -20,29 +20,28 @@ var auth = require(config.root+'/config/middlewares/authorization'),
     mongoose = require('mongoose'),
     database = require(config.root+'/config/database'),
     app = express();
-console.error(3);
+
 function runServer(){
 	//Bootstrap models
-  console.error(6);
+
 	var models_path = config.root + '/app/models';
 	fs.readdirSync(models_path).forEach(function(file) {
-    console.error(7,file);
       if(file.match(/^\..*/)){
         return;
       }
 	    require(models_path + '/' + file);
 	});
-  console.error(8);
+
 	//Bootstrap passport config
 	require(config.root+'/config/passport')(passport, config);
-console.error(9);
+
 	//Express settings
 	require(config.root+'/config/express')(app, config, passport);
 	//Bootstrap routes
- console.error(10);
+
 	require(config.root+'/config/routes')(app, passport, auth);
 	//Start the app by listening on <port>
-console.error(11);
+
 	app.listen(config.port);
 	console.log('Express app started on port ' + config.port);
 
@@ -51,10 +50,9 @@ console.error(11);
 //首先链接数据库，然后运行服务器
 database(function(err){
 	if(err){
-    console.error(4);
 		console.error(err.message,err.stack);
 	}else{
-    console.error(5);
+
 		runServer()
 	}
 })
