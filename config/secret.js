9c135175e26bd18714819f8f1ee914f40843f5fe
windwhinny <windwhinny@gmail.com>
2014-01-17 10:12:39 +0800

    build for github

--- config/secret.js
+++ config/secret.js
@@ -1,10 +1,9 @@
 module.exports={
-  imageAK:'1hiE4FNoplKf9OnmVEg3Sr6KXp3nBa2wDOIYv5f1',
-  imageSK:'2QtaTVv7z9yZQOGkKvLJqGWLHSKsI4eO-J5CI5rM',
-  baeAK:'mS9Y24GNkIzBPgWuRO1mnLxG',
-  baeSK:'n4pvaGqqFITu9YHmWXs6xxyw6PDQMajw',
-  mongoURL:'mongo.duapp.com',
-  mongoName:'zqZCsprmOaNetxiSoMcy',
-  mongoPort:8908
-
+  imageAK:'qiniu AK',
+  imageSK:'qiniu SK',
+  baeAK:'bae AK',
+  baeSK:'bae SK',
+  mongoURL:'mongo URL',
+  mongoName:'mongo databse name',
+  mongoPort:'port'
 }
