0d1aaa7a8dcb82a3456b110e70cb380cbddd7268
windwhinny <windwhinny@gmail.com>
2014-01-17 09:03:28 +0800

    test for bae 3

--- server.js
+++ server.js
@@ -1,59 +1,7 @@
-/**
- * Module dependencies.
- */
- 
-var express = require('express'),
-    fs = require('fs'),
-    passport = require('passport'),
-    path=require('path');
-
-if (process.env.BAE_ENV_APPID) {
- 	config = require('./config/config.js');
- }else{
- 	config = require('./config/config.js');
- }
-
-/**
- * Main application entry file.
- * Please note that the order of loading is important.
- */
-
-var auth = require(config.root+'/config/middlewares/authorization'),
-    mongoose = require('mongoose'),
-    database = require(config.root+'/config/database'),
-    app = express();
-
-function runServer(){
-	//Bootstrap models
-	var models_path = config.root + '/app/models';
-	fs.readdirSync(models_path).forEach(function(file) {
-      if(file.match(/^\..*/)){
-        return;
-      }
-	    require(models_path + '/' + file);
-	});
-	//Bootstrap passport config
-	require(config.root+'/config/passport')(passport, config);
-
-	//Express settings
-	require(config.root+'/config/express')(app, config, passport);
-	//Bootstrap routes
- 
-	require(config.root+'/config/routes')(app, passport, auth);
-	//Start the app by listening on <port>
-
-	app.listen(config.port);
-	console.log('Express app started on port ' + config.port);
-
-}
-
-//首先链接数据库，然后运行服务器
-database(function(err){
-	if(err){
-		console.error(err.message,err.stack);
-	}else{
-		runServer()
-	}
-})
-
-exports = module.exports = app;
+var http = require('http');
+var port = 18080;
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<h1>Node.js</h1>');
+    res.end('<p>Hello World</p>');
+}).listen(port);
